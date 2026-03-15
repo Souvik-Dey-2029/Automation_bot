ae83642d807d051e4893631d1fb45cf5cc6b7a5b
Souvik-Dey-2029 <souvikbwn12@gmail.com>
2026-03-15 18:21:30 +0530

    Step 9.4: Update Screenshot_loader.py

--- modules/screenshot_logger.py
+++ modules/screenshot_logger.py
@@ -1,19 +1,29 @@
 from modules.logger import log
 import pyautogui as pya
+from modules.config_loader import load_config
 import os
 import time
 from datetime import datetime
 
+
+config = load_config()
+
+folder = config["screenshot_folder"]
+interval = config["screenshot_interval"]
+
+
 def screenshot_logger():
-    folder="SS"
-    os.makedirs(folder,exist_ok=True)
+
+    os.makedirs(folder, exist_ok=True)
     print("Screenshot logger is getting started.......")
-    time.sleep(3)
+
     while True:
+
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         filename = f"{folder}/screenshot_{timestamp}.png"
         screenshot = pya.screenshot()
         screenshot.save(filename)
         print(f"Screenshot saved: {filename}")
         log(f"Screenshot saved: {filename}")
-        time.sleep(60)
+
+        time.sleep(interval)

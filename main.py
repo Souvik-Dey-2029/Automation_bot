02e4277b1028164ae2cb5d667e7911a5a894ec0b
Souvik-Dey-2029 <souvikbwn12@gmail.com>
2026-03-15 12:36:34 +0530

    Step 2: Coding environment launcher automation added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from modules.opener import open_notepad
+from modules.launcher import launch_environment
 
 print("Automation is getting Started................")
 open_notepad()
+launch_environment()
---
+++ modules/launcher.py
@@ -0,0 +1,22 @@
+import pyautogui as pya
+import webbrowser as web
+import time
+
+def launch_environment():
+    
+    print("Launching Coding Environmment")
+
+    pya.press("win")
+    time.sleep(1)
+    pya.write("Visual Studio Code",interval=0.1)
+    pya.press("enter")
+    time.sleep(3)
+
+    pya.press("win")
+    time.sleep(1)
+    pya.write("Chrome",interval=0.1)
+    pya.press("enter")
+    time.sleep(3)
+
+    web.open("https://chatgpt.com/")
+    web.open("https://github.com/")

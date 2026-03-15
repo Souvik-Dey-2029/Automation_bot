f37ba68708ab01ceb69420a168ae96b3d98a258d
Souvik-Dey-2029 <souvikbwn12@gmail.com>
2026-03-15 16:23:45 +0530

    Step 4: Added download monitoring automation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from modules.opener import open_notepad
 from modules.launcher import launch_environment
 from modules.organizer import organize_downloads
+from modules.monitor import monitor_downloads
 
 print("Automation is getting Started................")
 open_notepad()
 launch_environment()
 organize_downloads()
+monitor_downloads()
---
+++ modules/monitor.py
@@ -0,0 +1,28 @@
+import os
+import time
+from modules.organizer import organize_downloads
+
+
+def monitor_downloads():
+
+    downloads_path = r"C:\Users\SOUVIK\Downloads"
+
+    print("Download Monitor Started...")
+    print("Watching for new files...\n")
+
+    previous_files = set(os.listdir(downloads_path))
+
+    while True:
+
+        time.sleep(10)  # check every 10 seconds
+
+        current_files = set(os.listdir(downloads_path))
+
+        # detect new files
+        new_files = current_files - previous_files
+
+        if new_files:
+            print("New file detected:", new_files)
+            organize_downloads()
+
+        previous_files = current_files

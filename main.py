1c24a9503f3614a615f6903b12d43f255920f72f
Souvik-Dey-2029 <souvikbwn12@gmail.com>
2026-03-15 17:41:19 +0530

    Step-6: Multithreaded Automation System

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from modules.organizer import organize_downloads
 from modules.monitor import monitor_downloads
 from modules.screenshot_logger import screenshot_logger
 
-print("Automation is getting Started................")
+import threading
+
+print("Smart Automation is getting Started................\n")
+
+# intial setup
 open_notepad()
 launch_environment()
 organize_downloads()
-monitor_downloads()
-screenshot_logger()
+
+# creating Threads
+download_thread = threading.Thread(targent=monitor_downloads)
+Screenshot_thread = threading.Thread(targent=screenshot_logger)
+
+# Starting Threads
+download_thread.start()
+Screenshot_thread.start()

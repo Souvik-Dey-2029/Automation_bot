81683437a0c554ffadeb92424156f5bbc2215412
Souvik-Dey-2029 <souvikbwn12@gmail.com>
2026-03-15 17:43:33 +0530

    Step 7: Added CLI command system for automation control

--- main.py
+++ main.py
@@ -6,17 +6,58 @@ from modules.screenshot_logger import screenshot_logger
 
 import threading
 
-print("Smart Automation is getting Started................\n")
 
-# intial setup
-open_notepad()
-launch_environment()
-organize_downloads()
+def start_monitor():
+    thread = threading.Thread(target=monitor_downloads)
+    thread.start()
 
-# creating Threads
-download_thread = threading.Thread(targent=monitor_downloads)
-Screenshot_thread = threading.Thread(targent=screenshot_logger)
 
-# Starting Threads
-download_thread.start()
-Screenshot_thread.start()
+def start_screenshot():
+    thread = threading.Thread(target=screenshot_logger)
+    thread.start()
+
+
+def run_full_automation():
+
+    open_notepad()
+    launch_environment()
+    organize_downloads()
+
+    start_monitor()
+    start_screenshot()
+
+
+while True:
+
+    print("\nSMART AUTOMATION BOT\n")
+
+    print("1 → Launch Coding Environment")
+    print("2 → Organize Downloads")
+    print("3 → Start Download Monitor")
+    print("4 → Start Screenshot Logger")
+    print("5 → Run Full Automation")
+    print("0 → Exit\n")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        launch_environment()
+
+    elif choice == "2":
+        organize_downloads()
+
+    elif choice == "3":
+        start_monitor()
+
+    elif choice == "4":
+        start_screenshot()
+
+    elif choice == "5":
+        run_full_automation()
+
+    elif choice == "0":
+        print("Exiting bot...")
+        break
+
+    else:
+        print("Invalid option")
